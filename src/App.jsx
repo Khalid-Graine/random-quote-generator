7e6e4c04f0cf907fdd145eeb08bc6e13f598a691
Khalid Graine <khalidgraine1@gmail.com>
2023-11-13 10:23:31 +0100

    Design the page

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,19 @@ function App() {
   }, [random]); 
   return (
     <>
-      <div className='bg-gray-200 text-center p-2'>
-        <p> {advice.text}</p>
-        <p> {advice.author}</p>
-        <button onClick={() => setRandom(Math.floor(Math.random() * 15))} className='bg-black text-white px- py-1 rounded-sm'>Get Random Advice</button>
+      <div className='bg-[rgb(105,212,176)] h-screen text-[#0a0445] text-center p-2 md:w-6/12  mx-auto flex flex-col justify-center '>
+
+        <div className='bg-white border-[2px] border-r-4 border-b-4 border-[#0a0445] rounded-sm'>
+          <div className='text-start px-2 py-3 font-semibold bg-[#fdf099]'>
+        {advice.author}
+          </div>
+          <div className='px-2 py-6 text-start'>
+          {advice.text}
+          </div>
+        </div>
+      
+       
+        <button onClick={() => setRandom(Math.floor(Math.random() * 15))} className='bg-black mt-3 text-white px- py-1 rounded-sm'>Get Random Advice</button>
       </div>
     </>
   );
