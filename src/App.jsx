51aa88c72a620f1c66e9dad05fdd524a11836053
Khalid Graine <khalidgraine1@gmail.com>
2023-11-13 10:42:11 +0100

    Formating the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [advice, setAdvice] = useState('');
-  const [random,setRandom] = useState(Math.floor(Math.random() * 15));
-  const api = 'https://type.fit/api/quotes';
-
+  const [advice, setAdvice] = useState("");
+  const [random, setRandom] = useState(Math.floor(Math.random() * 15));
+  const api = "https://type.fit/api/quotes";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(api);
-        setAdvice(response.data[random]);
+        const data = response.data[random];
+        setAdvice({
+          text: data.text,
+          author: data.author.split(",")[0],
+        });
       } catch (error) {
-        console.error('Error fetching advice:', error);
+        console.error("Error fetching advice:", error);
       }
     };
 
     fetchData();
-  }, [random]); 
+  }, [random]);
   return (
     <>
-      <div className='bg-[rgb(105,212,176)] h-screen text-[#0a0445] text-center p-2 md:w-6/12  mx-auto flex flex-col justify-center '>
-
-        <div className='bg-white border-[2px] border-r-4 border-b-4 border-[#0a0445] rounded-sm'>
-          <div className='text-start px-2 py-3 font-semibold bg-[#fdf099]'>
-        {advice.author}
+      <div className="bg-[rgb(105,212,176)] text-lg h-screen text-[#0a0445] text-center p-2 md:w-6/12  mx-auto flex flex-col justify-center ">
+        <div className="bg-white the-border ">
+          <div className="text-start px-2 py-3 font-semibold bg-[#fdf099]">
+            {advice.author}
           </div>
-          <div className='px-2 py-6 text-start'>
-          {advice.text}
-          </div>
+          <div className="px-2 py-6 text-start">{advice.text}</div>
         </div>
-      
-       
-        <button onClick={() => setRandom(Math.floor(Math.random() * 15))} className='bg-black mt-3 text-white px- py-1 rounded-sm'>Get Random Advice</button>
+
+        <button
+          onClick={() => setRandom(Math.floor(Math.random() * 15))}
+          className=" mt-3 text-white font-bold text-xl px- py-2   the-border hover:bg-[#0a0445] active:bg-[#0a0460]"
+        >
+          Get Random Advice
+        </button>
       </div>
     </>
   );
